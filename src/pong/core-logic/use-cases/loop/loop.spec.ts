d936b0c3c47fb246e5df57966c758820e811883d
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-07-28 20:58:25 +0200

    feat: 🎸 Ball + Game + bounce if direction < Pi/2

--- src/pong/core-logic/use-cases/loop/loop.spec.ts
+++ src/pong/core-logic/use-cases/loop/loop.spec.ts
@@ -1,22 +1,23 @@
 import { describe, it, expect } from "vitest";
+import { Ball } from "../../value-objects/ball";
+import { Game } from "../../value-objects/game";
 
-type TPosition = {
+export type TPosition = {
   x: number;
   y: number;
 };
 
 describe("loop test", () => {
-  const tableSize: { length: number; height: number } = {
-    length: 300,
-    height: 200,
-  };
-
+  const tableSize: [number, number] = [300, 200];
+  //
   it.each`
-    ballPosition        | ballDirection  | ballSpeed | ballNextExpectedPosition
-    ${{ x: 20, y: 10 }} | ${0}           | ${1}      | ${{ x: 21, y: 10 }}
-    ${{ x: 20, y: 10 }} | ${Math.PI / 2} | ${1}      | ${{ x: 20, y: 11 }}
-    ${{ x: 20, y: 10 }} | ${Math.PI}     | ${1}      | ${{ x: 19, y: 10 }}
-    ${{ x: 20, y: 10 }} | ${Math.PI * 3/2}     | ${1}      | ${{ x: 20, y: 9 }}
+    ballPosition           | ballDirection        | ballSpeed | ballNextExpectedPosition                                                | expectedBallDirection
+    ${{ x: 20, y: 10 }}    | ${0}                 | ${1}      | ${{ x: 21, y: 10 }}                                                     | ${0}
+    ${{ x: 20, y: 10 }}    | ${Math.PI / 2}       | ${1}      | ${{ x: 20, y: 11 }}                                                     | ${Math.PI / 2}
+    ${{ x: 20, y: 10 }}    | ${Math.PI}           | ${1}      | ${{ x: 19, y: 10 }}                                                     | ${Math.PI}
+    ${{ x: 20, y: 10 }}    | ${(Math.PI * 3) / 2} | ${1}      | ${{ x: 20, y: 9 }}                                                      | ${(Math.PI * 3) / 2}
+    ${{ x: 299.5, y: 10 }} | ${0}                 | ${1}      | ${{ x: 299.5, y: 10 }}                                                  | ${Math.PI}
+    ${{ x: 299.5, y: 10 }} | ${Math.PI / 4}       | ${1}      | ${{ x: 2 * tableSize[0] - (299.5 + Math.cos(Math.PI / 4) * 1), y: 10 + Math.sin(Math.PI/4) * 1 }} | ${Math.PI - Math.PI / 4}
   `(
     "should move the ball",
     ({
@@ -24,26 +25,19 @@ describe("loop test", () => {
       ballDirection,
       ballSpeed,
       ballNextExpectedPosition,
+      expectedBallDirection,
     }: {
       ballPosition: TPosition;
       ballDirection: number;
       ballSpeed: number;
       ballNextExpectedPosition: TPosition;
+      expectedBallDirection: number;
     }) => {
-      expect(
-        calculateNextPosition(ballPosition, ballDirection, ballSpeed)
-      ).toEqual(ballNextExpectedPosition);
+      const ball = new Ball(ballPosition, ballDirection, ballSpeed);
+      const game = new Game(ball, tableSize);
+      game.move();
+      expect(ball.position).toEqual(ballNextExpectedPosition);
+      expect(ball.direction).toEqual(expectedBallDirection);
     }
   );
 });
-
-const calculateNextPosition = (
-  ball: TPosition,
-  direction: number,
-  speed: number
-): TPosition => {
-    return {
-      x: ball.x + Math.cos(direction) * speed,
-      y: ball.y + Math.sin(direction) * speed,
-    };
-};
