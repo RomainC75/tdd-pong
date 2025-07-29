0baf1fce1cd773ef2d04e191d92269e0c0e0c0b8
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-07-29 21:18:11 +0200

    feat: 🎸 move basics

--- src/pong/core-logic/use-cases/loop/loop.spec.ts
+++ src/pong/core-logic/use-cases/loop/loop.spec.ts
@@ -1,14 +1,49 @@
 import { describe, it, expect } from "vitest";
+import { expectedSnake, initSnake } from "./cases";
 
-type TPosition = {
-  x: number,
-  y: number
-}
+export type TPosition = {
+  x: number;
+  y: number;
+};
+
+export type Snake = {
+  positions: TPosition[];
+  direction: number;
+};
+
+
 
 describe("loop test", () => {
-  it("should move the snake", ()=>{
-    const snake: TPosition = [
-      []
-    ]
-  });
+  it.each`
+    initSnake    | expectedSnake
+    ${initSnake} | ${expectedSnake}
+  `(
+    "should move the snake",
+    ({
+      initSnake,
+      expectedSnake,
+    }: {
+      initSnake: Snake;
+      expectedSnake: Snake;
+    }) => {
+      const newSnake = move(initSnake);
+      expect(newSnake).to.deep.equal(expectedSnake)
+    }
+  );
 });
+
+const move = (snake: Snake): Snake => {
+  let snakePositionBuff: TPosition = snake.positions[0];
+  if (snake.direction === 0) {
+    snake.positions[0] = snake.positions[0] = {
+      x: snake.positions[0].x + 1,
+      y: snake.positions[0].y,
+    };
+  }
+  for (let i = 1; i < snake.positions.length; i++) {
+    const buff = snake.positions[i];
+    snake.positions[i] = snakePositionBuff;
+    snakePositionBuff = buff;
+  }
+  return snake;
+};
