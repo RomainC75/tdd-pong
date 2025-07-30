b48d989464372979ba2f2b084d2778d81f9c70ac
Romain CHENARD <romainchenard@Romains-MacBook-Pro.local>
2025-07-30 21:34:10 +0200

    refactor: 💡 shallow copy problem

--- src/pong/core-logic/use-cases/loop/loop.spec.ts
+++ src/pong/core-logic/use-cases/loop/loop.spec.ts
@@ -1,25 +1,37 @@
 import { describe, it, expect } from "vitest";
 import { expectedSnake, initSnake } from "./cases";
-import { RightDirectionState, Snake } from "../../value-objects/snake";
-
+import {
+  LeftDirectionState,
+  RightDirectionState,
+  Snake,
+  State,
+} from "../../value-objects/snake";
 
 describe("loop test", () => {
+  
+  it("should init and go to the right", () => {
+    const snake = new Snake([...initSnake.positions]);
+    snake.move();
+    expect(snake.positions).to.deep.equal(expectedSnake.positions);
+  });
+
   it.each`
-    initSnake    | expectedSnake
-    ${initSnake} | ${expectedSnake}
+  initSnakee    | expectedSnakee    | directionState
+  ${initSnake} | ${expectedSnake} | ${LeftDirectionState}
   `(
     "should move the snake",
     ({
-      initSnake,
-      expectedSnake,
+      initSnakee,
+      expectedSnakee,
+      directionState,
     }: {
-      initSnake: Snake;
-      expectedSnake: Snake;
+      initSnakee: Snake;
+      expectedSnakee: Snake;
+      directionSnake: State;
     }) => {
-      const snake = new Snake(initSnake.positions);
-
+      const snake = new Snake([...initSnakee.positions]);
       snake.move();
-      expect(snake.positions).to.deep.equal(expectedSnake.positions)
+      expect(snake.positions).to.deep.equal(expectedSnakee.positions);
     }
   );
 });
